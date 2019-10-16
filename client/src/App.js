23e9fc500f6a159a92e1307529e2586e3937ef6f
Hunter5555 <hunter.m.allan@gmail.com>
2019-10-16 15:10:59 -0600

    hunter allan mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SavedList from './Movies/SavedList';
+import MovieList from '../src/Movies/MovieList';
+import SavedList from "./Movies/SavedList";
+import Movie from '../src/Movies/Movie';
+import { Route } from "react-router-dom";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -12,7 +15,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/Movie/:MovieID" component={Movie} />
     </div>
   );
 };
